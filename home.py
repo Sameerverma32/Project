f8a1b2cea0f9752fad7578a9966948b5dbd5c223
Sameerverma32 <sameerverma8318@gmail.com>
2025-03-08 16:40:43 +0530

    tt

--- home.py
+++ home.py
@@ -10,6 +10,4 @@ import matplotlib.pyplot as plt
 
 st.markdown('<h1 style="color: black; font-size: bold; text-align: center; background-color:grey; margin:20px: border: 2px dashed yellow; font-family:sans serif">Weather EDA With Python<h1>', unsafe_allow_html=True)
 
-image_url = "https://png.pngtree.com/template/20211103/ourmid/pngtree-weather-forecast-business-style-simple-blue-template-image_710402.png"
-
-st.markdown(f"<div style='text-align:center;'><img src='{image_url}'></div>", unsafe_allow_html=True)
+st.image('assest\EDA66.png')
